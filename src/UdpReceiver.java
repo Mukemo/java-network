01ef24d5b13807bc3f298c0bcb3350070e50236d
germain <musulgermain@gmail.com>
2025-10-22 10:59:28 +0300

    adding more length for thr mrssage to prevent packet lost.

--- src/UdpReceiver.java
+++ src/UdpReceiver.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class UdpReceiver {
+
+    public static void main(String[] args) {
+
+        try(DatagramSocket ds = new DatagramSocket(3333)) {
+            DatagramPacket packet = new DatagramPacket(new byte[16], 16);
+            ds.receive(packet);
+            for(byte b:  packet.getData()) {
+                System.out.println(Character.valueOf((char)b));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/UdpSender.java
+++ src/UdpSender.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class UdpSender {
+
+    public static void main(String[] args) {
+        try(DatagramSocket ds = new DatagramSocket()) {
+            String msg = "Hello, this is UDP sender!";
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.length(), address, 3333);
+            ds.send(dp);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
